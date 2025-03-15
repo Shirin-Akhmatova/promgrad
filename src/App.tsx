c9ebd768623fbd0c168bc5b2c01ee33d50a7e05d
Ruslan <r.alisherovdev@gmail.com>
2025-03-15 17:25:30 +0600

    add burger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
 import "./assets/styles/main.scss";
+// import valleysImage from "./assets/images/valleys.jpg";
 
 const App = () => {
   return (
     <div>
       <Header />
+      {/* <img src={valleysImage} alt="nature" /> */}
     </div>
   );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,31 +12,51 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
+        {/* Бургер-меню */}
+        <div className={styles.burger}>
+          <Hamburger toggled={isMenuOpen} toggle={toggleMenu} />
+        </div>
+
         <a href="#" className={styles.logo}>
           LOGO
         </a>
 
         <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
           <ul>
-            <li>
+            <li className={styles.ulist}>
               <a href="#about">Главная</a>
+              <img
+                src="/src/assets/icons/Vector.svg"
+                alt="About Icon"
+                className={styles.icon}
+              />
             </li>
-            <li>
+            <li className={styles.ulist}>
               <a href="#services">О нас</a>
+              <img
+                src="/src/assets/icons/Vector.svg"
+                alt="About Icon"
+                className={styles.icon}
+              />
             </li>
-            <li>
+            <li className={styles.ulist}>
               <a href="#portfolio">Наши работы</a>
+              <img
+                src="/src/assets/icons/Vector.svg"
+                alt="About Icon"
+                className={styles.icon}
+              />
             </li>
-            <li>
+            <li className={styles.ulist}>
               <a href="#contact">Контакты</a>
+              <img
+                src="/src/assets/icons/Vector.svg"
+                alt="About Icon"
+                className={styles.icon}
+              />
             </li>
           </ul>
         </nav>
-
-        {/* Бургер-меню с помощью hamburger-react */}
-        <div className={styles.burger}>
-          <Hamburger toggled={isMenuOpen} toggle={toggleMenu} />
-        </div>
 
         <div className={styles.language}>RU</div>
       </div>
