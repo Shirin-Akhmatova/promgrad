d5b2bf02fe7a0229fb77c52187d2a7c8c0013ea1
Ruslan <r.alisherovdev@gmail.com>
2025-03-17 14:03:43 +0600

    final version of header/mainSlider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import Header from "./components/header/Header";
-import "./assets/styles/main.scss";
-import MainSlider from "./components/mainSlider/MainSlider";
-
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <MainSlider />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
