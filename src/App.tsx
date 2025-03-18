73354346d7fc5d75d415b2ae832f01005043a9e3
Ruslan <r.alisherovdev@gmail.com>
2025-03-18 12:30:20 +0600

    fix: Combine MainSlider and Footer, improve layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Footer from "./components/Footer/Footer";
+import Header from "./components/header/Header";
+import MainSlider from "./components/mainSlider/MainSlider";
 
 const App = () => {
   return (
     <div>
+      <Header />
+      <MainSlider />
       <Footer />
     </div>
   );
