e4e2fde686bc43bc51a1b4ccc17f4eb6413d9e87
Ruslan <r.alisherovdev@gmail.com>
2025-03-25 11:02:36 +0600

    added ky

---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+
+// // import en from "./locales/en.json";
+// // import ru from "./locales/ru.json";
+// // import ky from "./locales/ky.json";
+
+// i18n.use(initReactI18next).init({
+//   resources: {
+//     en: { translation: en },
+//     ru: { translation: ru },
+//     ky: { translation: ky },
+//   },
+//   lng: "en", // Язык по умолчанию
+//   fallbackLng: "en", // Если перевод для текущего языка не найден, будет использоваться этот
+//   interpolation: {
+//     escapeValue: false, // Не экранировать значения
+//   },
+// });
+
+// export default i18n;
