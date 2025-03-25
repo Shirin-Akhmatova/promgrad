94b7246c40f9300c5888389226b3fa9b9305942b
Ruslan <r.alisherovdev@gmail.com>
2025-03-25 19:39:56 +0600

    added whatsapp

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Header from "./components/header/Header";
 import MainSlider from "./components/mainSlider/MainSlider";
 import OurWork from "./components/ourWork/OurWork";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import "./App.scss";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <Header />
@@ -15,6 +20,16 @@ const App = () => {
       <OurWork />
       <OurContacts />
       <Footer />
+
+      <FloatingWhatsApp
+        phoneNumber="+996505230605"
+        accountName={t("whatsapp.accountName")}
+        avatar="https://old.avangardstyle.kg/wp-content/uploads/2020/03/Spanish-house-main.jpg"
+        allowClickAway={true}
+        chatMessage={t("whatsapp.chatMessage")}
+        placeholder={t("whatsapp.placeholder")}
+        className="whatsapp-button"
+      />
       <ToastContainer style={{ zIndex: 1000000 }} />
     </div>
   );
