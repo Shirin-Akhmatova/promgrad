98f0e2396a3ad29e0ecb5ed960133419c9385f39
Ruslan <r.alisherovdev@gmail.com>
2025-03-26 00:56:08 +0600

    changed modal icons

--- src/components/ourWork/modal/Modal.tsx
+++ src/components/ourWork/modal/Modal.tsx
@@ -1,5 +1,10 @@
 import styles from "./Modal.module.scss";
 import "./Modal.scss";
+import left from "../../../assets/icons/Arrow_left.svg";
+import right from "../../../assets/icons/Arrow_right.svg";
+import business from "../../../assets/icons/business.svg";
+import location from "../../../assets/icons/location.svg";
+import clock from "../../../assets/icons/clock.svg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
@@ -79,25 +84,17 @@ const Modal = ({ project, onClose }: ModalProps) => {
             ))}
           </Swiper>
           <button className={styles.customPrev}>
-            <img
-              src="/src/assets/icons/Arrow_left.svg"
-              alt="About Icon"
-              className={styles.icon}
-            />
+            <img src={left} alt="About Icon" className={styles.icon} />
           </button>
           <button className={styles.customNext}>
-            <img
-              src="/src/assets/icons/Arrow_right.svg"
-              alt="About Icon"
-              className={styles.icon}
-            />
+            <img src={right} alt="About Icon" className={styles.icon} />
           </button>
         </div>
         <div className={styles.modal_infos}>
           <h1 className={styles.modal_card_name}>{title}</h1>
           <p className={`${styles.modal_info} ${styles.modal_info_type}`}>
             <img
-              src="/src/assets/icons/business.svg"
+              src={business}
               alt="business Icon"
               className={styles.info_icon}
             />
@@ -105,18 +102,14 @@ const Modal = ({ project, onClose }: ModalProps) => {
           </p>
           <p className={`${styles.modal_info} ${styles.modal_info_address}`}>
             <img
-              src="/src/assets/icons/location.svg"
+              src={location}
               alt="location Icon"
               className={styles.info_icon}
             />
             <span>{address}</span>
           </p>
           <p className={`${styles.modal_info} ${styles.modal_info_date}`}>
-            <img
-              src="/src/assets/icons/clock.svg"
-              alt="clock Icon"
-              className={styles.info_icon}
-            />
+            <img src={clock} alt="clock Icon" className={styles.info_icon} />
             <span>{end_date}</span>
           </p>
           <h2 className={styles.modal_description}>{description}</h2>
